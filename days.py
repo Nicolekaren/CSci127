68a3423c79bf8aaa82c419bfeb4331315d799680
Nicolekaren <63335049+Nicolekaren@users.noreply.github.com>
2020-04-19 23:54:29 -0400

    Update days.py

--- days.py
+++ days.py
@@ -1,6 +1,6 @@
 #CSci 127 Teaching Staff
 #A program that uses functions to print out days.
-#Modified by:  ADD YOUR NAME HERE
+#Modified by:  Nicole Chiguil
 
 def dayString(dayNum):
      """
@@ -10,14 +10,28 @@ def dayString(dayNum):
      Assumes that input is an integer ranging from 1 to 7
      """
      
-     dayString = ""
+     dayString = def dayString(no):
+                    if no==1:
+                          print("Monday")
+                    elif no==2:
+                         print("Tuesday")
+                    elif no==3:
+                         print("Wednesday")
+                    elif no==4:
+                         print("Thrusday")
+                    elif no==5:
+                          print("Friday")
+                    elif no==6:
+                         print("Saturday")
+                    elif no==7:
+                         print("Sunday")
 
-     ###################################
-     ### FILL IN YOUR CODE HERE      ###
-     ### Other than your name above, ###
-     ### this is the only section    ###
-     ### you change in this program. ###
-     ###################################
+no=int(input("Enter day number:"))
+
+dayString(no)
+"
+
+   
 
      return(dayString)
 
